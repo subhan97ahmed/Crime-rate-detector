c4c9d7e32fc0ffa7c46d41c94790917bf7425f4f
subhan <subhan97ahmed@gmail.com>
2021-06-28 12:34:14 +0500

    start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import { Image } from 'antd';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,9 +18,16 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+    <Image
+      width={200}
+      src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+    />
+ 
+
+{/* ReactDOM.render(<ImageDemo />, mountNode);
     </div>
-  );
+  ); */}
 }
 
 export default App;
