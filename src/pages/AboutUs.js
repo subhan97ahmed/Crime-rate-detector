4dd22be02972230d941b170b1f30d9b18f8cede8
fiza ahmed <fiza.1601260@gmail.com>
2021-07-09 00:28:43 -0700

    update

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -10,12 +10,12 @@ const { Title } = Typography;
 const { Header, Content, Footer } = Layout;
 
 const txtColor = "#6aceef"
-const footerBgColor ="#2E2A2B"
+const footerBgColor = "#2E2A2B"
 const footerTxtColor = "#FFFFFF"
 function AboutUs() {
     return (
         <>
-            <Layout>
+            <Layout style={{ color: txtColor }}>
                 <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
                     <div className="logo" style={{ overflow: 'hidden' }}>
                         <Image src={log1} height={100} width={100} style={{ marginTop: "-20px" }} preview={false} ></Image>
@@ -31,28 +31,8 @@ function AboutUs() {
                         <div style={{
                             position: 'absolute', left: '50%', top: '50%',
                             transform: 'translate(-50%, -50%)', height: '40%', width: '70%',
-                        }}>
-                            {/*TODO: have to add about us information */}
-                            <section>
-  <div class="log_img">
-    <br> <br><br>
-    <div class="box1">
-        <h1 style="text-align: center; font-size: 35px;font-family: Lucida Console;">
-        Mentors Name</h1>
-        <h1 style="text-align: center;font-size: 20px;">Sameer Ahmed Siddiqui</h1>
-        <h1 style="text-align: center;font-size: 20px;">Ovais Ashraf</h1>
-        <h1 style="text-align: center; font-size: 35px;font-family: Lucida Console;">
-        Team B Members</h1>
-        <h1 style="text-align: center;font-size: 20px;">Subhan Ahmed</h1>
-        <h1 style="text-align: center;font-size: 15px;">subhan97ahmed@gmail.com</h1>
-        <h1 style="text-align: center;font-size: 20px;">Fiza Ahmed</h1>
-        <h1 style="text-align: center;font-size: 15px;">fiza.1601260@gmail.com</h1>
-        <h1 style="text-align: center;font-size: 20px;">Qaseem</h1>
-        <h1 style="text-align: center;font-size: 15px;">qaseemsamdani2000@gmail.com</h1>
-       </div>
-        
-  </div>
-</section>
+                        }}> 
+                        {/* asd */}
                         </div>
                     </div>
                 </Content>
