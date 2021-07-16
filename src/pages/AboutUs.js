550003b3d9acddf7693ed67e58385e0e878c98c1
qaseem2000 <qaseeemsamdani2000@gmail.com>
2021-07-16 02:30:27 -0700

    about us page fixed

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,65 +1,101 @@
-import "../App.less"
-import bg from '../bg.jpg'
-import { Layout, Typography } from 'antd';
-import NavBar from '../components/NavBar';
-import NavLogo from '../components/NavLogo'
+import "../App.less";
+import bg from "../bg.jpg";
+import { Layout, Typography, Image } from "antd";
+import NavBar from "../components/NavBar";
+import NavLogo from "../components/NavLogo";
+import log1 from "../logo1.png";
+import { Card } from "antd";
 //colors
-import * as conColors from '../colors'
+import * as conColors from "../colors";
 const { Title } = Typography;
 const { Header, Content, Footer } = Layout;
 
-
-const txtColor = conColors.txtColor
-const footerBgColor = conColors.footerBgColor
-const footerTxtColor = conColors.footerTxtColor
+const txtColor = conColors.txtColor;
+const footerBgColor = conColors.footerBgColor;
+const footerTxtColor = conColors.footerTxtColor;
 function AboutUs() {
-    return (
-        <>
-            <Layout className="aboutus">
-                <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
-                    <NavLogo></NavLogo>
-                    <NavBar selectedOp='3' />
-                </Header>
-                <Content className="site-layout" style={{ padding: '0 50px', marginTop: "64px", backgroundImage: `url(${bg})`, backgroundAttachment: "scroll" }}>
-                    <div style={{ margin: '16px 0' }}></div>
-                    <div className="site-layout-background" style={{ padding: "24px", minHeight: "780px", }}>
-                        <div style={{ textAlign: 'center' }}>
-                            <Title style={{ color: txtColor }}>About Us</Title>
-                        </div>
-                        <div style={{
-                            position: 'absolute', left: '50%', top: '50%',
-                            transform: 'translate(-50%, -50%)', height: '40%', width: '70%',
-                        }}>
-                            <section>
-                                <div className="log_img" >
-                                    <div className="box1" style={{color:txtColor }}>
-                                        <h1 style={{ textAlign: "center", fontSize: "35px" }}>
-                                            Mentors Name</h1>
-                                        <h1 style={{ textAlign: " center", fontSize: "25px" }}>Sameer Ahmed Siddiqui</h1>
-                                        <h1 style={{ textAlign: "center", fontSize: "25px" }}>Ovais Ashraf</h1>
-                                        <h1 style={{ textAlign: "center", fontSize: "35px" }}>
-                                            Team B Members</h1>
-                                            {/* <h1 style={{ textAlign: "center", fontSize: "25px",wordSpacing:"100px" }}>Name Email</h1> */}
-                                            <h1 style={{ textAlign: "center", fontSize: "25px",wordSpacing:"25px" }}>Areej  areej.zahid.023@gmail.com</h1>
-                                        <h1 style={{ textAlign: "center", fontSize: "25px",wordSpacing:"25px" }}>Fiza fiza.1601260@gmail.com</h1>
-                                        <h1 style={{ textAlign: "center", fontSize: "25px",wordSpacing:"25px" }}>Owais  Muhammadowaischandio@gmail.com</h1>
-                                        <h1 style={{ textAlign: "center", fontSize: "25px",wordSpacing:"25px" }}>Qaseem  qaseemsamdani2000@gmail.com</h1>
-                                        <h1 style={{ textAlign: "center", fontSize: "25px",wordSpacing:"25px" }}>Subhan  subhan97ahmed@gmail.com</h1>
-                                        
-                                        
-                                    
-                                        <h1 style={{ textAlign: "center", fontSize: "35px" }}></h1>
-                                    </div>
-
-                                </div>
-                            </section>
-                        </div>
-                    </div>
-                </Content>
-                <Footer style={{ textAlign: 'center', backgroundColor: footerBgColor, color: footerTxtColor }}>Design ©2021 Created by Team B</Footer>
-            </Layout>
-        </>
-    );
+  return (
+    <>
+      <Layout className="aboutus">
+        <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
+          <NavLogo></NavLogo>
+          <NavBar selectedOp="3" />
+        </Header>
+        <Content
+          className="site-layout"
+          style={{
+            padding: "0 50px",
+            marginTop: "64px",
+            backgroundImage: `url(${bg})`,
+            backgroundAttachment: "scroll",
+          }}
+        >
+          <div style={{ margin: "16px 0" }}></div>
+          <div
+            className="site-layout-background"
+            style={{ padding: "24px", minHeight: "780px" }}
+          >
+            <div style={{ textAlign: "center" }}>
+              <Title style={{ color: txtColor }}>About Us</Title>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                left: "50%",
+                top: "50%",
+                transform: "translate(-50%, -50%)",
+                height: "40%",
+                width: "70%",
+              }}
+            >
+              <section>
+                <div className="site-card-border-less-wrapper">
+                  <Card
+                    title="Crime rate detector"
+                    bordered={false}
+                    headStyle={{ fontSize: '35px', color:'#6DDAF2'}}
+                    style={{
+                      textAlign: "center",
+                      position: "absolute",
+                      left: "50%",
+                      top: "60%",
+                      transform: "translate(-50%, -50%)",
+                      height: "120%",
+                      width: "100%",
+                      overflow: "hidden",
+                      backgroundColor: footerBgColor,
+                    }}
+                  >
+                    <p style={{ textAlign: "center", fontSize: "20px", color:'#6DDAF2'}}>
+                      This is one of the interesting computer science project
+                      ideas to create. As the name suggests, this Computer
+                      Science project involves building a prediction system that
+                      can analyze and predict the crime rate of a particular
+                      location. Naturally, the system needs to be fed with
+                      relevant data. It uses the K-means data mining algorithm
+                      to predict the crime rate. The K-means algorithm can
+                      cluster co-offenders and organized crime groups by
+                      detecting relevant crime patterns via hidden links, link
+                      prediction, and statistical analysis of crime data.
+                    </p>
+                  </Card>
+                </div>
+              </section>
+            </div>
+          </div>
+        </Content>
+        <Footer
+          style={{
+            textAlign: "center",
+            backgroundColor: footerBgColor,
+            color: footerTxtColor,
+          }}
+        >
+          Design ©2021 Created by Team B
+        </Footer>
+      </Layout>
+    </>
+  );
 }
 
-export default AboutUs
+export default AboutUs;
