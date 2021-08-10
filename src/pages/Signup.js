bf707c063cbcc8090adfedcfc320748e9c38dffb
subhan <subhan97ahmed@gmail.com>
2021-08-10 18:48:40 +0500

    updated App.less added signup and login page in it and cleaned Signup.js

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -17,40 +17,7 @@ const footerBgColor = conColors.footerBgColor;
 const footerTxtColor = conColors.footerTxtColor;
 
 const { Option } = Select;
-// const residences = [
-//   {
-//     value: "zhejiang",
-//     label: "Zhejiang",
-//     children: [
-//       {
-//         value: "hangzhou",
-//         label: "Hangzhou",
-//         children: [
-//           {
-//             value: "xihu",
-//             label: "West Lake",
-//           },
-//         ],
-//       },
-//     ],
-//   },
-//   {
-//     value: "jiangsu",
-//     label: "Jiangsu",
-//     children: [
-//       {
-//         value: "nanjing",
-//         label: "Nanjing",
-//         children: [
-//           {
-//             value: "zhonghuamen",
-//             label: "Zhong Hua Men",
-//           },
-//         ],
-//       },
-//     ],
-//   },
-// ];
+
 const formItemLayout = {
   labelCol: {
     xs: {
